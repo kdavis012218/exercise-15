a666f5fb19a868106b8b6d6a7b749fd34d9d4223
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-25 20:54:33 -0500

    exercise 15

---
+++ ConsoleApp72/Exercise 15.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            int squaresolution;
+            
+            {
+                Console.WriteLine("Please enter a number");
+                Console.ReadLine();
+                
+                for (int cubesolution = input* input * input; counter++)
+                {
+                    Console.WriteLine(counter * counter * counter);
+                    Console.WriteLine("Number :{0}, Square : {1}", counter, counter * counter * counter);
+                }
+                Console.WriteLine("Would you like to continue?");
+                Console.ReadLine();
+
+            }
+        }
+}
